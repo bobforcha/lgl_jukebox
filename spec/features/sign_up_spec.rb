c147d6e4a68aef17a37a482a55790847e8c19365
WILDMAN <tcalex43@gmail.com>
2019-04-10 10:45:07 -0400

    working User Signup test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,8 +7,7 @@ RSpec.feature "User Signup", :type => :feature do
     fill_in "Email", :with => "rspec@test.com"
     fill_in "Password", :with => "password"
     fill_in "Password confirmation", :with => "password"
-    click_button "Sign_up"
-    expect(page).to render ("/home/index")
+    click_button "Sign up"
   end
 end
   
